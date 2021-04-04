b59b1f21f0ddfc091dccb2ed87c7d4089adad082
이현주 <ihyeonju@ihyeonjuui-MacBookPro.local>
2021-04-04 21:50:10 +0900

    공유버튼 추가(주석) 및 카카오맵 키 수정

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -15,8 +15,10 @@ function KakaoMap({ latitude, longitude }: KakaoMapProps) {
   useEffect(() => {
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_REST_API_KEY}&autoload=false`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_JAVASCRIPT_API_KEY}&autoload=false`;
     document.head.appendChild(script);
+    console.log("api 주소 호출")
+    console.log(process.env.REACT_APP_KAKAO_REST_API_KEY)
 
     script.onload = () => {
       window.kakao.maps.load(() => {
